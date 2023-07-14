c2328075a3cba0ec1b49f54fc2aa440229bc8845
Ana Beatriz Carvalho <ana.bcarvalho@souunit.com.br>
2023-07-14 09:31:58 -0300

    Remoção dos espaços vazios e letras sozinhas

--- modules/preprocess/pre_process_text.py
+++ modules/preprocess/pre_process_text.py
@@ -1,24 +1,26 @@
-'''
+"""
     Código para executar pré-processamento dos textos
 
-'''
+"""
 
-import spacy 
+import spacy
 from unidecode import unidecode
 
 
-class PreProcessText():
-
+class PreProcessText:
     def __init__(self, model):
-        
-        self.nlp_model = spacy.load(model)    
-
+        self.nlp_model = spacy.load(model)
 
     def process_text(self, text):
-
         doc_ = self.nlp_model(text)
-        
-        tokens = [unidecode(token.lemma_.lower()) for token in doc_ if not token.is_stop and not token.is_punct]
+        # adicionei "" para remover as palavras vazias e len(token.lemma_.strip()) > 1 para remover palavras com 1 caracter
+        tokens = [
+            unidecode(token.lemma_.lower().strip())
+            for token in doc_
+            if not token.is_stop
+            and not token.is_punct
+            and token.lemma_.strip() != ""
+            and len(token.lemma_.strip()) > 1
+        ]
 
         return tokens
-        
