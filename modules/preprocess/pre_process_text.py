e03c70a2407b9024ec93892026bf7750ef6d749d
Ana Beatriz Carvalho <ana.bcarvalho@souunit.com.br>
2023-11-18 22:10:10 -0300

    Adaptação do Pré-Processamento a receber vários diários oficiais

--- modules/preprocess/pre_process_text.py
+++ modules/preprocess/pre_process_text.py
@@ -6,37 +6,30 @@ class PreProcessText:
     def __init__(self, model):
         self.nlp_model = spacy.load(model)
 
-    # def is_date_format(self, text):
-    #     return (
-    #         len(text) == 10
-    #         and text[2] == "/"
-    #         and text[5] == "/"
-    #         and text[:2].isdigit()
-    #         and text[3:5].isdigit()
-    #         and text[6:].isdigit()
-    #     )
+        # def is_date_format(self, text):
+        #     return (
+        #         len(text) == 10
+        #         and text[2] == "/"
+        #         and text[5] == "/"
+        #         and text[:2].isdigit()
+        #         and text[3:5].isdigit()
+        #         and text[6:].isdigit()
+        #     )
 
     def process_text(self, text):
         doc_ = self.nlp_model(text.lower())
-
-        tokens = []
-        words = []
-        for token in doc_:
-            words.append(token)
-            if (
-                not token.is_stop  # Remove stopwords
-                and not token.is_punct  # Remove pontuação
-                and token.lemma_.strip() != ""  # Remove palavras vazias
-                and len(token.lemma_.strip())
-                > 5  # Remove falavras de tamanaho menor que 5
-            ):
-                if not (
-                    any(char.isdigit() for char in token.text)
-                    # and not self.is_date_format(token.text)
-                ):
-                    tokens.append(unidecode(token.lemma_.strip()))
-
-        return tokens
+        tokens = [
+            token.text.lower()
+            for token in doc_
+            if not (
+                token.is_stop
+                or token.is_punct
+                or token.lemma_.strip() == ""
+                or len(token.lemma_.strip()) <= 5
+                or any(char.isdigit() for char in token.text)
+            )
+        ]
+        return " ".join(tokens)
 
 
 if __name__ == "__main__":
