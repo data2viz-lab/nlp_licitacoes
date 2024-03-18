82d26a095594126afb900215ee0e9ea7c43c5c2d
erick <erickmarck.dev@gmail.com>
2024-03-18 09:23:10 -0300

    Retirado teste do codigo contador de termos por paginas

--- modules/utils/process_gazette.py
+++ modules/utils/process_gazette.py
@@ -33,29 +33,5 @@ class ProcessGazette():
 
         return dictionary_pages
     
-if __name__ == "__main__":
-    def word_counter(directory, file):
-        pg = ProcessGazette(directory)
-        pg.break_pages(file + ".txt","ANO [X|V|I]+ ",True)
-        
-        with open(directory+file+'_pages.json', 'r') as f:
-            data = json.load(f)
-        
-        for page, content in data.items():
-            print(f"Top 10 palavras na página {page+1}:")
-            word_count = {}
-            words = content.split()
-            for word in words:
-                if word in word_count:
-                    word_count[word] += 1
-                else:
-                    word_count[word] = 1
 
-            sorted_word_count = sorted(word_count.items(), key=lambda item: item[1], reverse=True)
-            for word, count in sorted_word_count[:10]:
-                print(f"{word}: {count}")
-            print("\n")
-        
-    
-    word_count = word_counter('notebooks/gazettes/', '2927408_20230704_0')
     
