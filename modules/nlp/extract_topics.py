ff20a7cedff6f5c0d4ddba7176bdffc245b98cdc
erick <erickmarck.dev@gmail.com>
2024-03-21 15:22:13 -0300

    Adicionado 'topics_info' para fornecer informações sobre tópicos. Aprimorada a funcionalidade permitindo o uso do modelo GPT.

--- modules/nlp/extract_topics.py
+++ modules/nlp/extract_topics.py
@@ -2,13 +2,27 @@ from bertopic import BERTopic
 
 class ExtractTopics():
 
-    def __init__(self):
+    def __init__(self,model='default'):
 
-        self.bert_model = BERTopic(language='portuguese', min_topic_size=2)
+        if model == 'default':
+            self.bert_model = BERTopic(language='portuguese', min_topic_size=2, nr_topics="auto")
+
+        elif model == 'gpt':
+            import openai
+            from bertopic.backend import OpenAIBackend
+
+            client = openai.OpenAI(api_key="sk-...")
+            embedding_model = OpenAIBackend(client, "text-embedding-ada-002")
+
+            topic_model = BERTopic(embedding_model=embedding_model, language='portuguese', min_topic_size=2, nr_topics="auto")
+
+
 
     def extract_topics(self, text):
 
-        model_ = self.bert_model.fit_transform(text)
-        topics = self.bert_model.get_topics(model_)
+        self.bert_model.fit_transform(text)
+        topics = self.bert_model.get_topics()
+        topics_info = self.bert_model.get_topic_info()
+        
 
-        return topics 
+        return topics, topics_info
