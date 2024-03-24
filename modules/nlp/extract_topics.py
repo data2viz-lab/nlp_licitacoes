7133b82a532f32cbb0bbff775277264f59373353
erick <erickmarck.dev@gmail.com>
2024-03-24 10:06:51 -0300

    Adicionado modelo zeroshot

--- modules/nlp/extract_topics.py
+++ modules/nlp/extract_topics.py
@@ -16,6 +16,21 @@ class ExtractTopics():
 
             topic_model = BERTopic(embedding_model=embedding_model, language='portuguese', min_topic_size=2, nr_topics="auto")
 
+        elif model == 'zeroshot':
+            from bertopic.representation import KeyBERTInspired
+
+            zeroshot_topic_list = ["Aquisições Diretas",
+                                    "Contratações Emergenciais",
+                                    "Dispensas de Licitação"]
+
+            self.bert_model = BERTopic(
+                embedding_model="thenlper/gte-small",
+                language="portuguese",
+                nr_topics="auto",
+                min_topic_size=2,
+                zeroshot_topic_list=zeroshot_topic_list,
+                zeroshot_min_similarity=.85,
+                representation_model=KeyBERTInspired())
 
 
     def extract_topics(self, text):
@@ -23,6 +38,8 @@ class ExtractTopics():
         self.bert_model.fit_transform(text)
         topics = self.bert_model.get_topics()
         topics_info = self.bert_model.get_topic_info()
+        #new_topics = topic_model.reduce_outliers(abstracts, topics)
+        #topic_model.update_topics(docs, topics=new_topics)
         
 
         return topics, topics_info
